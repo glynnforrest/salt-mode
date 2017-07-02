ca3fa1f4d43f604ffcb82e74a83ba28f2d6bb575
Glynn Forrest <me@glynnforrest.com>
2017-07-02 05:11:57 +0100

    Add temp buffer test macro helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,2 +1,11 @@
 (let ((project-dir (f-parent (f-dirname (f-this-file)))))
   (add-to-list 'load-path project-dir))
+(require 'salt-mode)
+
+(defmacro salt-test--with-buffer (contents &rest body)
+  "Run lisp forms BODY in a salt-mode buffer containing CONTENTS."
+  `(with-temp-buffer
+     (salt-mode)
+     (insert ,contents)
+     (goto-char (point-min))
+     ,@body))
