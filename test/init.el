d7c4ec05103fb84a5995627eaca6b956f1e341c1
Glynn Forrest <me@glynnforrest.com>
2017-07-02 02:42:28 +0100

    Use cask for test init.el

--- test/init.el
+++ test/init.el
@@ -1,17 +1,9 @@
 ;; A minimal init file to get salt-mode working for testing.
 
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
-
 (defvar salt-mode-root-dir (file-name-directory (directory-file-name (file-name-directory (or (buffer-file-name) load-file-name)))))
-(setq package-user-dir (concat salt-mode-root-dir "pkgs/"))
 
-(package-initialize)
-(package-refresh-contents)
-
-(dolist (pkg '(yaml-mode mmm-jinja2 package-lint))
-    (unless (package-installed-p pkg)
-  (package-install pkg)))
+(require 'cask "~/.cask/cask.el")
+(cask-initialize salt-mode-root-dir)
 
 ;; salt-mode should set yaml indentation without changing it globally
 ;; The default is 2 - the value of salt-mode-indent-level.
